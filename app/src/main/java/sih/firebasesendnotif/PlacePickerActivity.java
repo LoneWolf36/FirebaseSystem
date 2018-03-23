82298665d503f72a8ac54b6cdb7490f993b186a9
root <root@localhost.localdomain>
2018-03-24 03:24:06 +0530

    Maps added

--- app/src/main/java/sih/firebasesendnotif/PlacePickerActivity.java
+++ app/src/main/java/sih/firebasesendnotif/PlacePickerActivity.java
@@ -6,14 +6,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlacePicker;
 
+
 public class PlacePickerActivity extends AppCompatActivity {
     int PLACE_PICKER_REQUEST = 1;
+    TextView tvLat;
+    TextView tvLong;
     TextView tvPlace;
 
     @Override
@@ -21,6 +26,8 @@ public class PlacePickerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_place_picker);
         tvPlace= (TextView)findViewById(R.id.tvPlace);
+        tvLat= (TextView)findViewById(R.id.tvLat);
+        tvLong= (TextView)findViewById(R.id.tvLong);
     }
     public void goPlacePicker(View view) {
         PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
@@ -37,6 +44,8 @@ public class PlacePickerActivity extends AppCompatActivity {
                 Place place = PlacePicker.getPlace(PlacePickerActivity.this, data);
 
                 tvPlace.setText(place.getAddress());
+                tvLat.setText(place.getLatitude());
+                tvLong.setText(place.getLongitude());
             }
         }
 
