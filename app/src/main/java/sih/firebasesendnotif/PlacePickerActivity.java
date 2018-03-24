d7a97eed07b611514852dbe26e2936e3fc42d8f3
root <root@localhost.localdomain>
2018-03-24 13:08:15 +0530

    Fixed location

--- app/src/main/java/sih/firebasesendnotif/PlacePickerActivity.java
+++ app/src/main/java/sih/firebasesendnotif/PlacePickerActivity.java
@@ -1,7 +1,12 @@
 package sih.firebasesendnotif;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,14 +19,46 @@ import com.google.android.gms.location.places.ui.PlacePicker;
 
 public class PlacePickerActivity extends AppCompatActivity {
     int PLACE_PICKER_REQUEST = 1;
+    double Lat;
+    double Long;
     TextView tvPlace;
+    TextView tvLat;
+    TextView tvLong;
+    final private int REQUEST_CODE_ASK_PERMISSIONS = 123;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_place_picker);
         tvPlace= (TextView)findViewById(R.id.tvPlace);
+
     }
+//    private void permission() {
+//        if (ContextCompat.checkSelfPermission(PlacePickerActivity.this, Manifest.permission.) != PackageManager.PERMISSION_GRANTED) {
+//            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+//                new AlertDialog.Builder(this)
+//                        .setTitle("Permission required")
+//                        .setMessage("This permission is needed for accessing images from the storage")
+//                        .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+//                            @Override
+//                            public void onClick(DialogInterface dialog, int which) {
+//                                ActivityCompat.requestPermissions(PlacePickerActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, ACCESS_FINE_LOCATION_CODE);
+//                            }
+//                        })
+//                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+//                            @Override
+//                            public void onClick(DialogInterface dialog, int which) {
+//                                dialog.dismiss();
+//                            }
+//                        })
+//                        .create()
+//                        .show();
+//            } else
+//                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, EXTERNAL_STORAGE_REQUEST_CODE);
+//        }
+//    }
     public void goPlacePicker(View view) {
         PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
 
@@ -38,6 +75,8 @@ public class PlacePickerActivity extends AppCompatActivity {
                 Place place = PlacePicker.getPlace(PlacePickerActivity.this, data);
 
                 tvPlace.setText(place.getAddress());
+//                tvLong.setText(place.getAddress());
+//                tvLat.setText(place.getAddress());
             }
         }
 
