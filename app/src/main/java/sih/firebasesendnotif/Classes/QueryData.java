141a3556788f4a1ca1923b7b8201b168736930f2
Dhruvatara <dhruvatara.b@gmail.com>
2018-03-25 15:16:06 +0530

    contact auth added

---
+++ app/src/main/java/sih/firebasesendnotif/Classes/QueryData.java
@@ -0,0 +1,8 @@
+package sih.firebasesendnotif.Classes;
+
+/**
+ * Created by dhruvatara on 25/3/18.
+ */
+
+public class QueryData {
+}
--- app/src/main/java/sih/firebasesendnotif/Fragments/ContactAuthority.java
+++ app/src/main/java/sih/firebasesendnotif/Fragments/ContactAuthority.java
@@ -3,61 +3,40 @@ package sih.firebasesendnotif;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import sih.firebasesendnotif.Classes.QueryData;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link ContactAuthority.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link ContactAuthority#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ContactAuthority extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    FirebaseDatabase database= FirebaseDatabase.getInstance();
+    DatabaseReference ref=database.getReference("Queries");
+    Button submitQuery;
+    EditText city_name;
+    EditText queryContent;
     private OnFragmentInteractionListener mListener;
 
     public ContactAuthority() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ContactAuthority.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ContactAuthority newInstance(String param1, String param2) {
-        ContactAuthority fragment = new ContactAuthority();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -65,6 +44,24 @@ public class ContactAuthority extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_contact_authority, container, false);
+
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        submitQuery=view.findViewById(R.id.button1);
+        city_name=view.findViewById(R.id.editText1);
+        queryContent=view.findViewById(R.id.editText2);
+        submitQuery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("city",city_name.toString());
+                Log.d("query",queryContent.toString());
+                QueryData qdata=new QueryData(city_name.toString(),queryContent.toString());
+                ref.child(ref.push().getKey()).push();
+            }
+        });
     }
 
     // TODO: Rename method, update argument and hook method into UI event
